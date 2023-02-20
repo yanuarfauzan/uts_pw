8f31bf447701995a0d07d458278929e0cf4dcd4a
yanuarfauzan <yanuarisnain017@gmail.com>
2023-02-20 14:32:35 +0700

    memindahkan fil css yang di root ke folder bootstrap

--- layouts/header.php
+++ layouts/header.php
@@ -8,7 +8,7 @@
         <meta name="author" content="" />
         <title>Dashboard Perkuliahan</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-        <link href="../css/styles.css" rel="stylesheet" />
+        <link href="../bootstrap/css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     </head>
